a912e62c0d0100c046de2ed0ca9e04760148412b
deniPamungkas <pamungkasdeni18@gmail.com>
2022-11-22 15:52:37 +0700

    tambah pesan error

--- src/components/config.js
+++ src/components/config.js
@@ -3,26 +3,32 @@ const textInputConfig = [{
     name:"Username",
     type:"text",
     placeholder:"Username",
+    errorMessege:"Username should be 3-16 characters and shouldn't have any special character!"
 },{
     id:2,
     name:"Email",
     type:"email",
     placeholder:"Email",
+    errorMessege:"Email wrong"
 },{
     id:3,
     name:"Birthday",
     type:"date",
     placeholder:"Birthday",
+    errorMessege:"please fill this field"
+
 },{
     id:4,
     name:"Password",
     type:"password",
     placeholder:"Password",
+    errorMessege:"Password should have min.8 characters and should include uppercase, lowercase, and special character!"
 },{
     id:5,
     name:"ConfirmPassword",
     type:"password",
     placeholder:"Confirm Password",
+    errorMessege:"Password is not syncronous"
 },]
 
 export default textInputConfig
--- src/components/texInput.jsx
+++ src/components/texInput.jsx
@@ -5,6 +5,7 @@ const Input = (props) => {
   return (
     <label htmlFor={props.name} className="wrap-input">{props.name}
         <input id={props.name} name={props.name} type={props.type} placeholder={props.placeholder} value={props.value} onChange={props.onChange} pattern={props.pattern} required className='text-input'/>
+        <span>{props.errorMessege}</span>
     </label>
   )
 }
