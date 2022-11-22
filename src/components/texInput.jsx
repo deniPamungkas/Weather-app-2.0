026fc2e5d03e4f342b9aa01c78989d6fc5612ce0
deniPamungkas <pamungkasdeni18@gmail.com>
2022-11-22 16:41:21 +0700

    selesai

--- src/components/texInput.jsx
+++ src/components/texInput.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./textInput.scss"
 
 const Input = (props) => {
+  const [focused, setFocused] = useState(false)
   return (
     <label htmlFor={props.name} className="wrap-input">{props.name}
-        <input id={props.name} name={props.name} type={props.type} placeholder={props.placeholder} value={props.value} onChange={props.onChange} pattern={props.pattern} required className='text-input'/>
+        <input id={props.name} name={props.name} type={props.type} placeholder={props.placeholder} value={props.value} onChange={props.onChange} pattern={props.pattern} required className='text-input' onBlur={()=>setFocused(true)} focused={focused.toString()}/>
         <span>{props.errorMessege}</span>
     </label>
   )
